d277c6afc3ee3ab5e6cc22b5823a784bce5d3f36
marchelucie <lucie.pouponneau@live.fr>
2025-05-15 15:46:11 +0200

    created api routes
    get all books,
    get a book by id,
    post, put and delete a book
    wip : signup/signin

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 
 const Book = require('./models/Book');
+const User = require('./models/User');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://luciemarche:Loksdu489tn!@monvieuxgrimoire.unq6ero.mongodb.net/?retryWrites=true&w=majority&appName=MonVieuxGrimoire',
@@ -20,78 +21,66 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/api/books', (req, res, next) => {
-    const books = [
-        {
-            id: 1,
-            title: 'Le Petit Prince',
-            imageUrl: 'https://m.media-amazon.com/images/I/71IF1ngy57L._AC_UF1000,1000_QL80_.jpg',
-            averageRating: 4.5,
-            author: 'Antoine de Saint-Exupéry',
-            year: 1943,
-            genre: 'Fiction',
-        },
-        {
-            id: 2,
-            title: '1984',
-            imageUrl: 'https://m.media-amazon.com/images/I/7180qjGSgDL._AC_UF1000,1000_QL80_.jpg',
-            averageRating: 4.7,
-            author: 'George Orwell',
-            year: 1949,
-            genre: 'Dystopian',
-        },
-        {
-            id: 3,
-            title: 'To Kill a Mockingbird',
-            imageUrl: 'https://m.media-amazon.com/images/I/81gepf1eMqL._AC_UF1000,1000_QL80_.jpg',
-            averageRating: 4.8,
-            author: 'Harper Lee',
-            year: 1960,
-            genre: 'Fiction',
-        },
-    ];
-    res.status(200).json(books);
-    next();
+app.get('/api/books', (req, res) => {
+    Book.find()
+        .then(books => {
+            res.status(200).json(books);
+        })
+        .catch(error => res.status(400).json({ error }));
 });
 
-app.post('/api/books', (req, res, next) => {
+app.post('/api/books', (req, res) => {
     const book = new Book({
         ...req.body,
     });
     book.save()
         .then(() => res.status(201).json({ message: 'Book created' }))
         .catch(error => res.status(400).json({ error }));
-    next();
 });
 
-// app.post('/api/auth/signup', (req, res, next) => {
-//     delete req.body._id;
-//     const user = new User({
-//         ..req.body
-//     });
-//     user.save()
-//         .then(() => res.status(201).json({ message: 'User created' }))
-//         .catch(error => res.status(400).json({ error }));
-//     next();
-// });
+app.post('/api/auth/signup', (req, res) => {
+    // delete req.body._id;
+    const user = new User({
+        ...req.body
+    });
+    user.save()
+        .then(() => res.status(201).json({ message: 'User created' }))
+        .catch(error => res.status(400).json({ error }));
+});
 
-// app.get('/api/auth/signin', (req, res, next) => {
-//     User.find({ email: req.body.email })
-//         .then(user => {
-//             if (!user) {
-//                 return res.status(401).json({ error: 'User not found' });
-//             }
-//             if (user.password !== req.body.password) {
-//                 return res.status(401).json({ error: 'Incorrect password' });
-//             }
-//             res.status(200).json({
-//                 userId: user._id,
-//                 token: 'TOKEN',
-//             });
-//         })
-//         .catch(error => res.status(500).json({ error }));
-//     next();
-// }
-// );
+app.post('/api/auth/signin', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'User not found' });
+            }
+            if (user.password !== req.body.password) {
+                return res.status(401).json({ error: 'Incorrect password' });
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: 'TOKEN',
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res) => {
+    Book.findOne( { _id: req.params.id } )
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(500).json({ error }));
+});
+
+app.put('/api/books/:id', (req, res) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Book updated' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res) => {
+    Book.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Book deleted' }))
+        .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
