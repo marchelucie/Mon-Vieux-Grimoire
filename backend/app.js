dce10126046db6e5207c06801b30d25d1e04b77b
marchelucie <lucie.pouponneau@live.fr>
2025-05-16 12:05:40 +0200

    delete User (focus on Book)

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const app = express();
 app.use(express.json());
 
 const Book = require('./models/Book');
-const User = require('./models/User');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://luciemarche:Loksdu489tn!@monvieuxgrimoire.unq6ero.mongodb.net/?retryWrites=true&w=majority&appName=MonVieuxGrimoire',
@@ -36,33 +35,6 @@ app.post('/api/books', (req, res) => {
     book.save()
         .then(() => res.status(201).json({ message: 'Book created' }))
         .catch(error => res.status(400).json({ error }));
-});
-
-app.post('/api/auth/signup', (req, res) => {
-    // delete req.body._id;
-    const user = new User({
-        ...req.body
-    });
-    user.save()
-        .then(() => res.status(201).json({ message: 'User created' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.post('/api/auth/signin', (req, res) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'User not found' });
-            }
-            if (user.password !== req.body.password) {
-                return res.status(401).json({ error: 'Incorrect password' });
-            }
-            res.status(200).json({
-                userId: user._id,
-                token: 'TOKEN',
-            });
-        })
-        .catch(error => res.status(500).json({ error }));
 });
 
 app.get('/api/books/:id', (req, res) => {
