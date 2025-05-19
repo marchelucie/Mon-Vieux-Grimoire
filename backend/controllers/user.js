c4266049d04c5bb11cb55066867a174e2e0292ab
marchelucie <lucie.pouponneau@live.fr>
2025-05-19 15:55:24 +0200

    login controller created

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,5 +16,26 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user === null) {
+                res.status(401).json({ error: 'Identifiant et/ou mot de passe incorrect' });
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            res.status(401).json({ error: 'Identifiant et/ou mot de passe incorrect' });
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                token: 'TOKEN',
+                            });
+                        }
+                    })
+                    .catch(error =>
+                        res.status(500).json({ error }))
+            }
+        })
+        .catch(error =>
+            res.status(500).json({ error }))
 };
