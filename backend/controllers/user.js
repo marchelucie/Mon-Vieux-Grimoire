1ca53ea8834294452f37ece17d8669af1604d382
marchelucie <lucie.pouponneau@live.fr>
2025-05-20 10:18:39 +0200

    config user authentification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -28,7 +29,10 @@ exports.login = (req, res, next) => {
                         } else {
                             res.status(200).json({
                                 userId: user._id,
-                                token: 'TOKEN',
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24h' })
                             });
                         }
                     })
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const bookCtrl = require('../controllers/books');
 
 router.get('/', bookCtrl.getAllBooks);
-router.post('/', bookCtrl.createBook);
+router.post('/', auth, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
