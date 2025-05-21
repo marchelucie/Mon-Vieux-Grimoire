e8482bd8e39cd68990e656e90fcc76d905c52d42
marchelucie <lucie.pouponneau@live.fr>
2025-05-21 10:54:02 +0200

    deleted header and footer on the connection page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  BrowserRouter, Route, Routes,
+  BrowserRouter, Route, Routes, useLocation,
 } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Home from './pages/Home/Home';
@@ -13,27 +13,39 @@ import UpdateBook from './pages/updateBook/UpdateBook';
 import { useUser } from './lib/customHooks';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
-function App() {
+function AppContent() {
   const [user, setUser] = useState(null);
   const { connectedUser } = useUser();
+  const location = useLocation();
 
   useEffect(() => {
     setUser(connectedUser);
   }, [connectedUser]);
+
+  const hideLayoutRoutes = [APP_ROUTES.SIGN_IN];
+
+  const hideLayout = hideLayoutRoutes.includes(location.pathname);
+
+  return (
+    <>
+      <ScrollToTop />
+      {!hideLayout && <Header user={user} setUser={setUser} />}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
+        <Route path={APP_ROUTES.BOOK} element={<Book />} />
+        <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
+        <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
+      </Routes>
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
+
+function App() {
   return (
     <BrowserRouter>
-      <div>
-        <ScrollToTop />
-        <Header user={user} setUser={setUser} />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
-          <Route path={APP_ROUTES.BOOK} element={<Book />} />
-          <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
-          <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
